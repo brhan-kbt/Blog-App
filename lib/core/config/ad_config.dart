afe7e357e454eecf7ad3f496b6ada904cbf33886
Brhan <brhankbt63@gmail.com>
2025-09-13 10:50:59 +0300

    ad real ad configuration

--- lib/core/config/ad_config.dart
+++ lib/core/config/ad_config.dart
@@ -1,12 +1,12 @@
 class AdConfig {
   // Test Ad Unit IDs from Google documentation
   // Replace with your real IDs for release
-  static const androidBanner = 'ca-app-pub-5496551731411329/9293096079';
-  static const androidInterstitial = 'ca-app-pub-5496551731411329/7513110545';
-  static const androidRewarded = 'ca-app-pub-5496551731411329/6187570523';
-  static const androidAppOPen = 'ca-app-pub-5496551731411329/5726557980';
+  static const androidBanner = 'ca-app-pub-5496551731411329/2857686217';
+  static const androidInterstitial = 'ca-app-pub-5496551731411329/9323076118';
+  static const androidRewarded = 'ca-app-pub-5496551731411329/9264184607';
+  static const androidAppOPen = 'ca-app-pub-5496551731411329/3592183389';
   static const androidRewardedInterstitial =
-      'ca-app-pub-5496551731411329/4357635995';
+      'ca-app-pub-5496551731411329/6696912775';
 
   static const iosBanner = 'ca-app-pub-3940256099942544/2934735716';
   static const iosInterstitial = 'ca-app-pub-3940256099942544/4411468910';
