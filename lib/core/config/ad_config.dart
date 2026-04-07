2241820df78d3961bb4fb575b419926ce1c41b41
Brhan <brhankbt63@gmail.com>
2026-04-07 20:42:47 +0300

    change ad config

--- lib/core/config/ad_config.dart
+++ lib/core/config/ad_config.dart
@@ -3,20 +3,20 @@ class AdConfig {
   // Replace with your real IDs for release
 
   //Test
-  static const androidBanner = 'ca-app-pub-3940256099942544/9214589741';
-  static const androidInterstitial = 'ca-app-pub-3940256099942544/1033173712';
-  static const androidRewarded = 'ca-app-pub-3940256099942544/5224354917';
-  static const androidAppOPen = 'ca-app-pub-3940256099942544/9257395921';
-  static const androidRewardedInterstitial =
-      'ca-app-pub-3940256099942544/5354046379';
+  // static const androidBanner = 'ca-app-pub-3940256099942544/9214589741';
+  // static const androidInterstitial = 'ca-app-pub-3940256099942544/1033173712';
+  // static const androidRewarded = 'ca-app-pub-3940256099942544/5224354917';
+  // static const androidAppOPen = 'ca-app-pub-3940256099942544/9257395921';
+  // static const androidRewardedInterstitial =
+  //     'ca-app-pub-3940256099942544/5354046379';
 
   //Real
-  // static const androidBanner = 'ca-app-pub-5496551731411329/7344269679';
-  // static const androidInterstitial = 'ca-app-pub-5496551731411329/4732572294';
-  // static const androidRewarded = 'ca-app-pub-5496551731411329/9072284311';
-  // static const androidAppOPen = 'ca-app-pub-5496551731411329/54391 49301';
-  // static const androidRewardedInterstitial =
-  //     'ca-app-pub-5496551731411329/9207003926';
+  static const androidBanner = 'ca-app-pub-5496551731411329/7344269679';
+  static const androidInterstitial = 'ca-app-pub-5496551731411329/4732572294';
+  static const androidRewarded = 'ca-app-pub-5496551731411329/9072284311';
+  static const androidAppOPen = 'ca-app-pub-5496551731411329/5439149301';
+  static const androidRewardedInterstitial =
+      'ca-app-pub-5496551731411329/9207003926';
 
   static const iosBanner = 'ca-app-pub-3940256099942544/2934735716';
   static const iosInterstitial = 'ca-app-pub-3940256099942544/4411468910';
